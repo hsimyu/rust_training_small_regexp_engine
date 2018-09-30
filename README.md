464da718346cc72e5233d06f2a4d38af75a9426a
hsimyu <hsimyu1900@gmail.com>
2018-09-30 21:50:03 +0900

    ? 演算子のアサーションでコケる。

---
+++ README.md
@@ -0,0 +1,2 @@
+# What
+This training regexp engine is a Rust version of the tiny regexp engine described in this article: https://postd.cc/build-your-own-regex/
--- src/main.rs
+++ src/main.rs
@@ -34,12 +34,12 @@ fn main()
     assert_eq!(search("hogeg", "aaahogegaaa"), true);
 
     // ?
-    // assert_eq!(search("ab?c", "ac"), true);
-    // assert_eq!(search("ab?c", "abc"), true);
-    // assert_eq!(search("a?b?c", "abc"), true);
-    // assert_eq!(search("a?b?c?", ""), true);
-    // assert_eq!(search("a?b?c", "c"), false);
-    // assert_eq!(search("ab?c", "bc"), false);
+    assert_eq!(search("ab?c", "ac"), true);
+    assert_eq!(search("ab?c", "abc"), true);
+    assert_eq!(search("a?b?c", "abc"), true);
+    assert_eq!(search("a?b?c", "c"), true);
+    assert_eq!(search("ab?c", "bc"), false);
+    assert_eq!(search("a?b?c?", ""), true);
 }
 
 fn match_one(pattern: &str, text: &str) -> bool {
@@ -59,11 +59,7 @@ fn match_multi(pattern: &str, text: &str) -> bool {
 
     if pattern == "$" && text.is_empty() { return true; } // $ 文字サポート
 
-    if text.is_empty() { return false; } // テキストが空なら、パターンに依らず一致しない
-
     let (pattern_head, pattern_tail) = pattern.split_at(1);
-    let (text_head, text_tail) = text.split_at(1);
-
     // パターンが2文字以上残っている場合は ? 文字チェックのため、パターンの次も見る
     if pattern_tail.len() > 1 {
         let (pattern_second_head, _) = pattern_tail.split_at(1);
@@ -72,6 +68,9 @@ fn match_multi(pattern: &str, text: &str) -> bool {
             return match_question(pattern, text);
         }
     }
+
+    if text.is_empty() { return false; } // テキストが空なら、パターンに依らず一致しない
+    let (text_head, text_tail) = text.split_at(1);
 
     // ここに辿り着いたら通常の match
     return 
@@ -88,14 +87,20 @@ fn match_question(pattern: &str, text: &str) -> bool {
     // head は更に "文字" と "?" に分割
     let (pattern_head, _) = pattern_head.split_at(1);
 
-    // text の頭を抽出
-    let (text_head, text_tail) = text.split_at(1);
+    if !text.is_empty() {
+        // text の頭を抽出
+        let (text_head, text_tail) = text.split_at(1);
 
-    return 
-        // ?より前の文字が一致しており、?以降も一致している
-        (match_one(pattern_head, text_head) && match_multi(pattern_tail, text_tail)) ||
-        // ?以降の文字が一致している
-        match_multi(pattern_tail, text);
+        return 
+            // ?より前の文字が一致しており、?以降も一致している
+            (match_one(pattern_head, text_head) && match_multi(pattern_tail, text_tail)) ||
+            // ?以降の文字が一致している
+            match_multi(pattern_tail, text);
+    } else {
+        // text が空文字の時はパターンの後ろだけチェックすればいい
+        return match_multi(pattern_tail, text);
+    }
+
 }
 
 fn search(pattern: &str, text: &str) -> bool {
@@ -106,20 +111,20 @@ fn search(pattern: &str, text: &str) -> bool {
     } else {
         // ^ 文字で始まっていないパターンの場合は、
         // text の全地点を開始位置として検証
-        let res = (0..text.len()).any(
+
+        // text か空の場合は特別なことしない
+        if text.is_empty() {
+            println!("[0]: check match between {} and {}", pattern, text);
+            return match_multi(pattern, text);
+        }
+
+        return (0..text.len()).any(
             |index| -> bool {
                 let (_, text_tail) = text.split_at(index);
-
                 println!("[{}]: check match between {} and {}", index, pattern, text_tail);
+
                 return match_multi(pattern, text_tail);
             }
         );
-
-        if res {
-            println!("Matched!");
-        } else {
-            println!("Do Not matched.");
-        }
-        res
     }
 }
